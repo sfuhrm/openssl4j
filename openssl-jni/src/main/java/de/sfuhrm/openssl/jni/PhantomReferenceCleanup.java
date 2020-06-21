3a659ae0010e25cf33cc522d965f72f2e41cf00d
Stephan Fuhrmann <s@sfuhrm.de>
2020-06-21 20:45:23 +0200

    Javadoc

--- openssl-jni/src/main/java/de/sfuhrm/openssl/jni/PhantomReferenceCleanup.java
+++ openssl-jni/src/main/java/de/sfuhrm/openssl/jni/PhantomReferenceCleanup.java
@@ -5,16 +5,29 @@ import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.nio.ByteBuffer;
 
+/**
+ * Frees native ByteBuffer objects.
+ * The ByteBuffer objects are allocated in {@linkplain AbstractNative#AbstractNative()}
+ * and are not used any longer.
+ * @author Stephan Fuhrmann
+ */
 class PhantomReferenceCleanup {
+
+    /** The reference queue of unused ByteBuffer objects. */
     private static final ReferenceQueue<ByteBuffer> BYTE_BUFFER_REFERENCE_QUEUE = new ReferenceQueue<>();
 
+    /** Is the thread running? */
     private static boolean running = false;
 
+    /** Enqueues a ByteBuffer for later cleanup. */
     static void enqueueForCleanup(ByteBuffer bb) {
         PhantomReference<ByteBuffer> phantomReference = new PhantomReference<ByteBuffer>(bb, BYTE_BUFFER_REFERENCE_QUEUE);
         startIfNeeded();
     }
 
+    /** Checks whether the queue thread is already
+     * running and starts it if not.
+     * */
     static synchronized void startIfNeeded() {
         if (!running) {
             running = true;
