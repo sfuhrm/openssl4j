30420a8b91dfa72e50f18acbd7f53f22f76de4e0
Stephan Fuhrmann <s@sfuhrm.de>
2020-06-27 12:34:23 +0200

    Fix

--- .travis/travis-after_success.sh
+++ .travis/travis-after_success.sh
@@ -4,12 +4,12 @@
 # File is executed in travisci environment
 #
 
-sudo apt-get install -y xmlstarlet
+sudo apt-get install -y xmlstarlet || exit 10
 
 POM_VERSION="$(xmlstarlet sel -N p="http://maven.apache.org/POM/4.0.0" -t -v "/p:project/p:version/text()" pom.xml)"
 SSL_VERSION=$(cat target/ssl-lib)
 OSSL4JNAME=$(cat target/openssl4j-lib)
 
-curl -T target/${OSSL4JNAME} \
+curl -T ${OSSL4JNAME} \
 -usfuhrm:${BINTRAY_API_KEY} \
-https://api.bintray.com/content/sfuhrm/openssl4j/objects/${POM_VERSION}/libopenssl4j-$(uname -m)-$(uname -s)-${SSL_VERSION}
+https://api.bintray.com/content/sfuhrm/openssl4j/objects/${POM_VERSION}/libopenssl4j-$(uname -m)-$(uname -s)-${SSL_VERSION} || exit 10
