8afcb083d698667c275470a717560c5c2337bf07
Stephan Fuhrmann <s@sfuhrm.de>
2020-06-27 13:43:10 +0200

    Pull docker to top level

--- .travis/travis-after_success.sh
+++ .travis/travis-after_success.sh
@@ -8,8 +8,6 @@ sudo apt-get install -y xmlstarlet || exit 10
 
 SSL_VERSION=$(cat target/ssl-lib)
 OSSL4JNAME=$(cat target/openssl4j-lib)
-
-docker run --mount type=bind,source="$(pwd)",target=/build debian-adoptopenjdk:latest "/build/.travis/docker-pom-version.sh"
 POM_VERSION="$(cat target/pom.version)"
 
 curl -T ${OSSL4JNAME} \
