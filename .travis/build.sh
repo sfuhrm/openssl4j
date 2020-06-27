c6d9f7ffec9ed776d975f0d1349ef3d1ef9a5af5
Stephan Fuhrmann <s@sfuhrm.de>
2020-06-27 10:35:36 +0200

    Prepare to find java home

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,9 +7,17 @@ echo "\$TRAVIS_DIST: $TRAVIS_DIST"
 echo "\$TRAVIS_CPU_ARCH: $TRAVIS_CPU_ARCH"
 echo "\$JAVA_HOME: $JAVA_HOME"
 
+echo "JAVA location: " $(which java)
+echo "JAVAC location: " $(which javac)
+
 # first check some basics
 javac -version || exit 5
 make clean || exit 10
+
+if [ "x" -eq "x$JAVA_HOME" ]; then
+  echo "No JAVA_HOME set"
+  exit 10
+fi
 
 # build
 make || exit 10
