8dda58faffda41a77a781e06444c261375300d8b
Stephan Fuhrmann <s@sfuhrm.de>
2023-05-28 16:35:26 +0200

    Javadoc fixes for building with JDK 17

--- openssl4j/src/main/java/de/sfuhrm/openssl4j/package-info.java
+++ openssl4j/src/main/java/de/sfuhrm/openssl4j/package-info.java
@@ -3,13 +3,38 @@
  * native OpenSSL library is being linked dynamically
  * into the JVMs address space.
  *
- * <h3>Usage</h3>
+ * <h2>Usage</h2>
  * There are multiple ways of using the library.
  *
- * <h4>Runtime configuration</h4>
+ * <h3>Runtime configuration</h3>
+ *
+ * You can create an instance of a message digest as follows if you are
+ * explicitly specifying the crypto provider {@code OpenSSL4JProvider}:
+ *
  * {@code MessageDigest md5 = new MessageDigest("MD5", new OpenSSL4JProvider());}
  *
- * <h4>Runtime configuration</h4>
- * {@code MessageDigest md5 = new MessageDigest("MD5", new OpenSSL4JProvider());}
+ * <h3>JDK-wide configuration</h3>
+ *
+ * You can specify the {@code OpenSSL4JProvider} to be used JDK-wide
+ * implicitly by the by changing your
+ * <ul>
+ * <li>Linux, or macOS: `&lt;java-home&gt;/conf/security/java.security</li>
+ * <li>Windows: `&lt;java-home&gt;\conf\security\java.security</li>
+ *</ul>
+ *
+ * to have the OpenSSL4J provider in the first place:
+ *
+ * <code>
+ security.provider.1=OpenSSL4J
+ security.provider.2=SUN
+ security.provider.3=SunRsaSign
+ security.provider.4=SunEC
+ security.provider.5=SunJSSE
+ security.provider.6=SunJCE
+ security.provider.7=SunJGSS
+ security.provider.8=SunSASL
+ security.provider.9=XMLDSig
+ security.provider.10=SunPCSC
+ </code>
  */
 package de.sfuhrm.openssl4j;
--- openssl4j/src/main/java/module-info.java
+++ openssl4j/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 import de.sfuhrm.openssl4j.OpenSSL4JProvider;
 
+/**
+ * Provides the OpenSSL4j cryptographic provider implementation
+ * for use with the native OpenSSL dynamic library.
+ */
 module de.sfuhrm.openssl4j {
     provides java.security.Provider with OpenSSL4JProvider;
     exports de.sfuhrm.openssl4j to java.base;
